5d7756ac5985d48c10d27d44f5e6f7ae1b9bba9a
Miirshe <miirshe@gmail.com>
2023-10-20 18:54:50 +0300

    currently working country api explained than other person

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -14,7 +14,7 @@ const Countries = () => {
 		fetchData();
 
 	},[])
-
+console.log('country',country);
   return (
 	<div className="w-full mt-10">
 		<div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
@@ -22,12 +22,18 @@ const Countries = () => {
 			{
 				country?.map((res , index) => {
 					return(
-						<div className="w-full overflow-hidden flex flex-col justify-start items-start gap-2 space-y-3" key={index}>
-							<img src={res?.flags.png} alt="" />
+						<div className="w-full overflow-hidden flex flex-col justify-start items-start gap-2 space-y-3 
+						bg-slate-900 shadow-md rounded p-3" key={index}>
+							<img className="w-full h-44 bg-cover object-center" src={res?.flags.png} alt="" />
+							<hr />
 							<div className=" space-y-2">
-
+								<p className=" text-xl tracking-widest text-slate-500">Name : <span className="text-white text-lg">{res?.name?.common}</span></p>
+								<p className=" text-xl tracking-widest text-slate-500">Area : <span className="text-white text-lg">{res?.area}</span></p>
+								<p className=" text-xl tracking-widest text-slate-500">Population : <span className="text-white text-lg">{res?.population}</span></p>
+								<p className=" text-xl tracking-widest text-slate-500">Region : <span className="text-white text-lg">{res?.region}</span></p>
 							</div>
 						</div>
+
 					)
 				})
 			}
